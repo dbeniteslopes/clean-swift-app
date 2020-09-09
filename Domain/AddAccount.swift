834c164fb3c0b4f1e101373d536705fa29b62e58
Diego Benites Lopes <dbeniteslopes@gmail.com>
2020-09-09 11:18:51 -0300

    Changed domain models to public

--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-protocol AddAccount {
+public protocol AddAccount {
     func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
 }
 
-struct AddAccountModel {
+public struct AddAccountModel {
     var name: String
     var email: String
     var password: String
