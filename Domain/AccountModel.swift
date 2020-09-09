b319ca2cdb19e48ac0111a431621adb5a087c868
Diego Benites Lopes <dbeniteslopes@gmail.com>
2020-09-09 12:23:46 -0300

    Changed domain models to internal again because @testable

--- Domain/AccountModel.swift
+++ Domain/AccountModel.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct AccountModel {
+struct AccountModel {
     var id: String
     var name: String
     var email: String
--- Domain/AddAccount.swift
+++ Domain/AddAccount.swift
@@ -1,10 +1,10 @@
 import Foundation
 
-public protocol AddAccount {
+protocol AddAccount {
     func add(addAccountModel: AddAccountModel, completion: @escaping (Result<AccountModel, Error>) -> Void)
 }
 
-public struct AddAccountModel: Encodable {
+struct AddAccountModel: Encodable {
     var name: String
     var email: String
     var password: String
