e81b6de41491a971e6c39a18b33e91329f5a4d82
Diego Benites Lopes <dbeniteslopes@gmail.com>
2020-09-23 23:29:11 -0300

    Check memory leak

--- InfraTests/AlamoFireAdapterTests.swift
+++ InfraTests/AlamoFireAdapterTests.swift
@@ -35,12 +35,14 @@ class AlamofireAdapterTests: XCTestCase {
 
 extension AlamofireAdapterTests {
     
-    func makeSut() -> AlamofireAdapter {
+    func makeSut(file: StaticString = #file, line: UInt = #line) -> AlamofireAdapter {
         let configuration = URLSessionConfiguration.default
         configuration.protocolClasses = [UrlProtocolStub.self]
         let session = Session(configuration: configuration)
+        let sut = AlamofireAdapter(session: session)
+        checkMemoryLeak(for: sut, file: file, line: line)
         
-        return AlamofireAdapter(session: session)
+        return sut
     }
     
     func testRequestFor(url: URL = makeUrl(), data: Data?, action: @escaping (URLRequest) -> Void) {
