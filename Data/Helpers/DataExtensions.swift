e8c22a343b773bfd903b654e67e9b29b8ca40d6d
Diego Benites Lopes <dbeniteslopes@gmail.com>
2020-11-10 23:59:43 -0300

    Move AlamofireAdapter to its own file

--- Data/Helpers/DataExtensions.swift
+++ Data/Helpers/DataExtensions.swift
@@ -2,11 +2,11 @@ import Foundation
 
 extension Data {
     
-    func toModel<T: Decodable>() -> T? {
+    public func toModel<T: Decodable>() -> T? {
         return try? JSONDecoder().decode(T.self, from: self)
     }
     
-    func toJson() -> [String: Any]? {
+    public func toJson() -> [String: Any]? {
         return try? JSONSerialization.jsonObject(with: self, options: .allowFragments) as? [String: Any]
     }
 }
